271a8cc971e33a5fb12414e192c44fd037070d3d
Cosmina23 <91978964+Cosmina23@users.noreply.github.com>
2023-11-01 00:19:31 +0200

    Update sapt5.sh

--- sapt5.sh
+++ sapt5.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
-#-L/-h - legatura simbolica
-
-#grep -E '^[A-Z]{1,1}[a-zA-Z0-9 ,]*\.$' | grep -vE '(si[ ]*,)' | grep -v 'n[pb]'
 
 if [ "$#" -lt 2 ]
 then
     echo "Prea putine argumente"
 fi
 
-count=0
+count=0;
+
+echo "" > "$2"
 
 for entry in $1/*
 do
+
     if [ -f "$entry" ]
     then
-	r=$(cat "$entry" | grep -E '^[A-Z]{1,1}[a-zA-Z0-9 ,]*\.$' | grep -vE '(si[ ]*,)' | grep -v 'n[pb]')
+	r=$(cat "$entry" | grep -E '^[A-Z]{1,1}[a-zA-Z0-9 ,]*\.$' | grep -vE '(si[ ]*,)' | grep -v 'n[pb]');
 	if [[ -n $r ]]
 	then
 	    echo "$entry" >> "$2"
 	fi
-    
-    elif [ -L "$entry" ]
-    then
-	count='expr $count + 1'
-    
-    elif [ -d "$entry" ]
-    then
-	count2=$(bash $0 $file $2)
-	(count='expr $count + $count2')
     fi
-echo "$count"
-
+    if [ -L "$entry" ]
+    then
+	(( count++ ))
+    fi
+    if [ -d "$entry" ]
+    then
+	count2=$(bash $0 $entry $2)
+	count=$((count + count2))
+    fi
 done
+    
+
+echo "$count"	      
 
