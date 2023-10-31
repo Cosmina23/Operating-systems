9cd6a777bb89616aa34d2b98fcc62f9790d1ab05
Cosmina23 <91978964+Cosmina23@users.noreply.github.com>
2023-11-01 00:19:53 +0200

    .

--- tema1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#regex care verifica daca o data de forma dd-MM-yyyy hh:mm:ss e valida
-while read line
-do
-    r=$(echo "$line" | grep -E '^((0[1-9])|(1[0-9])|(2[0-9])|(3[0-1]))-((0[1-9])|(1[0-2]))-[0-9]{4}\ ([0-1][0-9]|2[0-4]):([0-5][0-9]):([0-5][0-9])' | grep -vE '^(31|30)-(02)-[0-9]{4}\ ([0-1][0-9]|2[0-4]):([0-5][0-9]):([0-5][0-9])' )
-    echo "$r"
-done 
