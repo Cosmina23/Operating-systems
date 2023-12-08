49e853b1dd8e107196c70cd5c223b5a38028ce54
Cosmina23 <91978964+Cosmina23@users.noreply.github.com>
2023-12-08 15:51:01 +0200

    Update script.sh

--- Proiect/saptamana9/script.sh
+++ Proiect/saptamana9/script.sh
@@ -3,11 +3,12 @@
 if [ "$#" -ne 1 ]
 then
     echo "Usage $0 <character>"
+    exit 1
 fi
 c=0
 while read linie
 do
-    r=$( echo "$linie" | grep -E '^[A-Z]{1,1}[a-zA-Z0-9\ \,\?\!\.]*(\.|\?|\!)$' | grep -vE '(\,[\ ]*si)')
+    r=$( echo "$linie" | grep -E '^[A-Z]{1,1}[a-zA-Z0-9\ \,\?\!\.]*(\.|\?|\!)$' | grep -vE '(\,[\ ]*si)' | grep -E "$1+")
     if [[ -n "$r" ]]
     then
 	(( c++ )) 
